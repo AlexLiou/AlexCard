bdc1b4745a6687bc1d15499ab2936321ce13aeb0
alexliou <alexliou0@gmail.com>
2022-08-26 17:08:23 -0500

    Initial Commit

---
+++ AlexCard/AlexCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlexCardApp.swift
+//  AlexCard
+//
+//  Created by Alex Liou on 8/26/22.
+//
+
+import SwiftUI
+
+@main
+struct AlexCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
